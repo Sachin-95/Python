c74daf682edff824b9bb6bface8caa7e2cc5f811
Sachin-95 <35479123+Sachin-95@users.noreply.github.com>
2018-02-21 19:14:49 +0530

    Update settings.py

--- Python/settings.py
+++ Python/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(_usf6vn_84o%*0y9==wf3x0ewp9@juri!047d15k_gey8eq&3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['172.17.*.*']
 
 
 # Application definition
