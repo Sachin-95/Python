5231307a94689afc91a4df81515cf6fe0e196a95
Sachin-95 <35479123+Sachin-95@users.noreply.github.com>
2018-02-23 14:38:29 +0530

    Update settings.py

--- Python/settings.py
+++ Python/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Python.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student',
+        'USER': 'Python_DB_Inst',
+        'PASSWORD': 'sachin123',
+        'HOST': 'connect-python.cd6dkhdncwjg.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
