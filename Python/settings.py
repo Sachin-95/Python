3e4d7b0da72b063c87cfbecd8e1acb565ec093a7
Sachin-95 <35479123+Sachin-95@users.noreply.github.com>
2018-02-27 21:06:31 +0530

    add env parameter

--- Python/settings.py
+++ Python/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'Python.wsgi.application'
 
 DATABASES = {
     'default': {
-        
-        'ENGINE': 'django.db.backends.mysql',
-        'PORT': '3306'
+           'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         
         
     }
