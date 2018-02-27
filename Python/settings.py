f73eefd4e2db2c5cd3a3682041e7658b67ce695f
Sachin-95 <35479123+Sachin-95@users.noreply.github.com>
2018-02-27 19:00:54 +0530

    Update settings.py

--- Python/settings.py
+++ Python/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         
         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student',
+        'USER': 'Python_DB_Inst',
+        'PASSWORD': 'sachin123',
+        'HOST': 'connect-python.cd6dkhdncwjg.us-east-1.rds.amazonaws.com',
         'PORT': '3306'
         
         
