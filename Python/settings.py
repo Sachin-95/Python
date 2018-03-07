bedb406603f348db4f251ee4a89b89135bd457dd
Sachin-95 <35479123+Sachin-95@users.noreply.github.com>
2018-03-07 12:21:10 +0530

    Update settings.py

--- Python/settings.py
+++ Python/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'Python.wsgi.application'
 DATABASES = {
     'default': {
            'ENGINE': 'django.db.backends.mysql',
-            'NAME': prod['RDS_DB_NAME'],
-            'USER': prod['RDS_USERNAME'],
-            'PASSWORD': prod['RDS_PASSWORD'],
-            'HOST': prod['RDS_HOSTNAME'],
-            'PORT': prod['RDS_PORT'],
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         
         
     }
